75bf259652b924a5b2933fb52b2d62340dacdc4a
Joshua Treudler <joshua@treudler.net>
2024-06-10 17:05:59 +0200

    update icons for menu

--- js/menu.js
+++ js/menu.js
@@ -23,6 +23,8 @@ let __menu_icons = {
 	"tstats": `<svg xmlns="http://www.w3.org/2000/svg" class="menu-icon" viewBox="0 0 36 36"><path fill="#CCD6DD" d="M31 2H5C3.343 2 2 3.343 2 5v26c0 1.657 1.343 3 3 3h26c1.657 0 3-1.343 3-3V5c0-1.657-1.343-3-3-3z"/><path fill="#E1E8ED" d="M31 1H5C2.791 1 1 2.791 1 5v26c0 2.209 1.791 4 4 4h26c2.209 0 4-1.791 4-4V5c0-2.209-1.791-4-4-4zm0 2c1.103 0 2 .897 2 2v4h-6V3h4zm-4 16h6v6h-6v-6zm0-2v-6h6v6h-6zM25 3v6h-6V3h6zm-6 8h6v6h-6v-6zm0 8h6v6h-6v-6zM17 3v6h-6V3h6zm-6 8h6v6h-6v-6zm0 8h6v6h-6v-6zM3 5c0-1.103.897-2 2-2h4v6H3V5zm0 6h6v6H3v-6zm0 8h6v6H3v-6zm2 14c-1.103 0-2-.897-2-2v-4h6v6H5zm6 0v-6h6v6h-6zm8 0v-6h6v6h-6zm12 0h-4v-6h6v4c0 1.103-.897 2-2 2z"/><path fill="#DD2E44" d="M4.998 33c-.32 0-.645-.076-.946-.239-.973-.523-1.336-1.736-.813-2.709l7-13c.299-.557.845-.939 1.47-1.031.626-.092 1.258.118 1.705.565l6.076 6.076 9.738-18.59c.512-.978 1.721-1.357 2.699-.843.979.512 1.356 1.721.844 2.7l-11 21c-.295.564-.841.953-1.47 1.05-.627.091-1.266-.113-1.716-.563l-6.1-6.099-5.724 10.631C6.4 32.619 5.71 33 4.998 33z"/></svg>`,
 	"armor": `<svg xmlns="http://www.w3.org/2000/svg" class="menu-icon" viewBox="0 0 36 36"><path fill="#CCD6DD" d="M33 3c-7-3-15-3-15-3S10 0 3 3C0 18 3 31 18 36c15-5 18-18 15-33z"/><path fill="#55ACEE" d="M18 33.884C6.412 29.729 1.961 19.831 4.76 4.444 11.063 2.029 17.928 2 18 2c.071 0 6.958.04 13.24 2.444 2.799 15.387-1.652 25.285-13.24 29.44z"/><path fill="#269" d="M31.24 4.444C24.958 2.04 18.071 2 18 2v31.884c11.588-4.155 16.039-14.053 13.24-29.44z"/></svg>`,
 	"map": `<img src="/data/img/map.png" class="menu-icon" alt="Map" />`,
+	"rankingarchive": `<img src="/data/img/rankingarchive.png" class="menu-icon" alt="Map" />`,
+
 }
 
 let __menu_content = `
@@ -41,7 +43,7 @@ let __menu_content = `
 			<div class="menuitem menu-dropdown-content">
 				<a href="wstats.html">${__menu_icons["wstats"]} ${_("WZ statistics")}</a>
 				<a href="https://poludnica.shinyapps.io/configs/" target="_blank">${__menu_icons["tstats"]} ${_("Trainer statistics")}${__menu_external_link}</a>
-				<a href="rankingarchive.html">${__menu_icons["wstats"]} ${_("Rankingarchive")}</a>
+				<a href="rankingarchive.html">${__menu_icons["rankingarchive"]} ${_("Rankingarchive")}</a>
 			</div>
 		</li>
 		<li class="menuitem bold menu-dropdown">
