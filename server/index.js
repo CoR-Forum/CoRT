7c285a887b9c79af90331a28754be6cd3beb1c0d
Joshua Treudler <joshua@treudler.net>
2024-07-12 22:30:32 +0200

    fix path

--- server/index.js
+++ server/index.js
@@ -264,7 +264,7 @@ function checkAuth(req, res, next) {
 
 // Routes
 // / will serve static files from ../ and /api/v1 will serve the rest api
-  app.use('/', express.static('../public'));
+  app.use('/', express.static('public'));
 
 // USERS
 // register - register a new user with email, password, username, nickname.
