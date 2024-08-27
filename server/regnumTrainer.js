6e8097e4bad918e9158fa84d6ecba2964ad459e1
Joshua Treudler <joshua@treudler.net>
2024-08-27 14:56:45 +0200

    remove old comments

--- server/regnumTrainer.js
+++ server/regnumTrainer.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const db = require('./dbInit');
+const logger = require('./winston');
+const checkAuth = require('./middleware/checkAuth');
+
 const router = express.Router();
-const db = require('./dbInit'); // Adjust the path as necessary
-const logger = require('./winston'); // Adjust the path as necessary
-const checkAuth = require('./middleware/checkAuth'); // Adjust the path as necessary
 
 const API_PATH = process.env.API_PATH || '/api/v1';
 
-// retrieve own trainer setups
 router.get('/trainer/mysetups', checkAuth, (req, res) => {
     db.query('SELECT * FROM trainer_setups WHERE user_id = ?', [req.session.userId], (err, result) => {
         if (err) {
@@ -18,8 +18,6 @@ router.get('/trainer/mysetups', checkAuth, (req, res) => {
     });
 });
 
-// retrieve all public trainer setups
-// include user nickname and id
 router.get(API_PATH + '/trainer/setups', (req, res) => {
     db.query('SELECT trainer_setups.*, users.nickname, users.id AS user_id FROM trainer_setups JOIN users ON trainer_setups.user_id = users.id WHERE is_public = TRUE', (err, result) => {
       if (err) {
@@ -29,6 +27,6 @@ router.get(API_PATH + '/trainer/setups', (req, res) => {
       }
       res.send(result);
     });
-  });
+});
 
 module.exports = router;
