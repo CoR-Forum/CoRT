de29906486712fd9710bbd71727dc88d8adec63e
Joshua Treudler <joshua@treudler.net>
2024-09-11 14:25:04 +0200

    add missing dot

--- public/js/menu.js
+++ public/js/menu.js
@@ -204,7 +204,7 @@ let __menu_footer = `
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-		<p><i>CoRT is a free and open source website.</i> This is a fork of the <a href="https://github.com/mascaldotfr/CoRT" target="_blank">original CoRT project</a></p>
+		<p><i>CoRT is a free and open source website.</i> This is a fork of the <a href="https://github.com/mascaldotfr/CoRT" target="_blank">original CoRT project</a>.</p>
 		<a href="https://github.com/CoR-Forum/CoRT/issues" target="_blank">report bugs</a>
 		<a href="https://github.com/CoR-Forum/CoRT" target="_blank">source code</a>
 		<a href="https://github.com/CoR-Forum/CoRT/blob/v2/CHANGELOG.md" target="_blank">Changelog</a>
